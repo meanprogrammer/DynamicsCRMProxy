942bd34a4e36bf63e252375b5edaf6719e056942
meanprogrammer <mean.programmer.d@gmail.com>
2016-01-14 17:31:19 +0800

    123213

--- DynamicsCRMProxy.Solution/CRMProxyService.Entity/ProxyCreditGuaranteeRequest.cs
+++ DynamicsCRMProxy.Solution/CRMProxyService.Entity/ProxyCreditGuaranteeRequest.cs
@@ -1,13 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using Xrm;
 
 namespace CRMProxyService.Entity
 {
     public class ProxyCreditGuaranteeRequest
     {
-       
+        [DataMember]
+        public string RequestNo { get; set; }
+        [DataMember]
+        public string IssuingBankName { get; set; }
+        [DataMember]
+        public string ConfirmingBankName { get; set; }
+        [DataMember]
+        public string TypeOfTradeTransaction { get; set; }
+        [DataMember]
+        public string ApplicantName { get; set; }
+        [DataMember]
+        public string BeneficiaryName { get; set; }
+        [DataMember]
+        public string Tenor { get; set; }
+        [DataMember]
+        public string Goods { get; set; }
+        [DataMember]
+        public double TotalTransactionValue { get; set; }
+        [DataMember]
+        public double ADBAmountCovered { get; set; }
     }
 }

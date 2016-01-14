edabb166a9fa688d083f225a35e62273a7406881
meanprogrammer <mean.programmer.d@gmail.com>
2016-01-14 16:54:03 +0800

    1234

---
+++ DynamicsCRMProxy.Solution/CRMProxyService/Services/ITradeFinanceService.cs
@@ -0,0 +1,20 @@
+﻿using CRMProxyService.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CRMProxyService.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ITradeFinanceService" in both code and config file together.
+    [ServiceContract]
+    public interface ITradeFinanceService
+    {
+        void AddTradeFinance(ProxyTradeFinance entity);
+        IEnumerable<ProxyTradeFinance> GetAllTradeFinanace();
+        ProxyTradeFinance GetOneTradeFinance();
+        void UpdateTradeFinance(ProxyTradeFinance eneity);
+    }
+}
---
+++ DynamicsCRMProxy.Solution/CRMProxyService/Services/TradeFinanceService.svc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CRMProxyService.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "TradeFinanceService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select TradeFinanceService.svc or TradeFinanceService.svc.cs at the Solution Explorer and start debugging.
+    public class TradeFinanceService : ITradeFinanceService
+    {
+
+        public void AddTradeFinance(Entity.ProxyTradeFinance entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Entity.ProxyTradeFinance> GetAllTradeFinanace()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Entity.ProxyTradeFinance GetOneTradeFinance()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTradeFinance(Entity.ProxyTradeFinance eneity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

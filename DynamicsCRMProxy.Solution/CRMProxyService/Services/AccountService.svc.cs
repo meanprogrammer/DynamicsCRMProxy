7781072a07164e3ce74564bb224b260ab9c50ee8
meanprogrammer <mean.programmer.d@gmail.com>
2016-07-21 10:45:40 +0800

    4312

--- DynamicsCRMProxy.Solution/CRMProxyService/Services/AccountService.svc.cs
+++ DynamicsCRMProxy.Solution/CRMProxyService/Services/AccountService.svc.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Activation;
 using System.Text;
 using Xrm;
 
@@ -12,6 +13,7 @@ namespace CRMProxyService.Services
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "AccountService" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select AccountService.svc or AccountService.svc.cs at the Solution Explorer and start debugging.
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
     public class AccountService : IAccountService
     {
 

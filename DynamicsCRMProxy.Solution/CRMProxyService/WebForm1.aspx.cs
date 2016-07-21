6d47c89cd5f0893da01dbd5364fd12aaefef5efa
meanprogrammer <mean.programmer.d@gmail.com>
2016-07-21 10:59:04 +0800

    44444

--- DynamicsCRMProxy.Solution/CRMProxyService/WebForm1.aspx.cs
+++ DynamicsCRMProxy.Solution/CRMProxyService/WebForm1.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CRMProxyService.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,9 @@ namespace CRMProxyService
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            AccountService service = new AccountService();
+            var x = service.GetAllConfirmingBanks();
+            Response.Write(x);
         }
     }
 }

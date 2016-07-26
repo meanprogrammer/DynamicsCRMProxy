2ceefbc189e83255c4963a7598f58518ab2d4e5f
meanprogrammer <mean.programmer.d@gmail.com>
2016-07-26 16:27:46 +0800

    11111

--- DynamicsCRMProxy.Solution/CRMProxyService/Services/TradeFinanceService.svc.cs
+++ DynamicsCRMProxy.Solution/CRMProxyService/Services/TradeFinanceService.svc.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Activation;
 using System.Text;
 using System.Web.UI;
 using Xrm;
@@ -13,6 +14,8 @@ namespace CRMProxyService.Services
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "TradeFinanceService" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select TradeFinanceService.svc or TradeFinanceService.svc.cs at the Solution Explorer and start debugging.
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    [ServiceBehavior(AddressFilterMode = AddressFilterMode.Any)]
     public class TradeFinanceService : ITradeFinanceService
     {
 

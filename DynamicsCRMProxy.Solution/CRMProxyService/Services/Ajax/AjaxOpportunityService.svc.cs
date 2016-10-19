ddd8f02c9e8ee3caac5fc583e0beebcd38e6c831
meanprogrammer <mean.programmer.d@gmail.com>
2016-10-19 10:12:18 +0800

    1234

---
+++ DynamicsCRMProxy.Solution/CRMProxyService/Services/Ajax/AjaxOpportunityService.svc.cs
@@ -0,0 +1,63 @@
+﻿using CRMProxyService.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+using Xrm;
+
+namespace CRMProxyService.Services.Ajax
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class AjaxOpportunityService : IAjaxOpportunityService
+    {
+        // To use HTTP GET, add [WebGet] attribute. (Default ResponseFormat is WebMessageFormat.Json)
+        // To create an operation that returns XML,
+        //     add [WebGet(ResponseFormat=WebMessageFormat.Xml)],
+        //     and include the following line in the operation body:
+        //         WebOperationContext.Current.OutgoingResponse.ContentType = "text/xml";
+        private XrmServiceContext xrm = null;
+        public AjaxOpportunityService()
+        {
+            xrm = new XrmServiceContext("Xrm");
+        }
+
+
+        public List<Entity.ProxyOpportunity> GetAllOpportunity()
+        {
+            List<ProxyOpportunity> accs = new List<ProxyOpportunity>();
+            //using (var xrm = new XrmServiceContext("Xrm"))
+            //{
+            var all = xrm.OpportunitySet;
+            foreach (Opportunity item in all)
+            {
+                ProxyOpportunity converted = ObjectConverter.ConvertToReadableOpportunity(item, this.xrm);
+                accs.Add(converted);
+            }
+            //}
+            return accs;
+        }
+
+        public Entity.ProxyOpportunity GetOneOpportunity(Guid id)
+        {
+            //var xrm = new XrmServiceContext("Xrm");
+            //return 
+            Opportunity orig = this.xrm.OpportunitySet.Where(c => c.Id == id).FirstOrDefault();
+
+            return ObjectConverter.ConvertToReadableOpportunity(orig, this.xrm);
+        }
+
+        public bool UpdateOpportunity(Guid id, Entity.ProxyOpportunity opp)
+        {
+            //var xrm = new XrmServiceContext("Xrm");
+            Xrm.Opportunity orig = this.xrm.OpportunitySet.Where(c => c.Id == id).FirstOrDefault();
+            orig.Description = opp.Description;
+            xrm.Update(orig);
+            return true;
+        }
+    }
+}
---
+++ DynamicsCRMProxy.Solution/CRMProxyService/Services/Ajax/Interface/IAjaxOpportunityService.cs
@@ -0,0 +1,26 @@
+﻿using CRMProxyService.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace CRMProxyService.Services
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IOpportunityService" in both code and config file together.
+    [ServiceContract]
+    public interface IAjaxOpportunityService
+    {
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        List<ProxyOpportunity> GetAllOpportunity();
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        ProxyOpportunity GetOneOpportunity(Guid id);
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        bool UpdateOpportunity(Guid id, ProxyOpportunity opp);
+    }
+}
